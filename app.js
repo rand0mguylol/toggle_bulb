b8459681d84aab6bf814a0a2540f1a869e3235f0
rand0mguylol <72346097+rand0mguylol@users.noreply.github.com>
2022-03-25 01:51:26 +0800

    second commit

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 (function () {
   document.getElementById("bulb").addEventListener("click", (e) => {
-    if(e.target.classList.contains("bulb-on")){
+
+    
+    if(e.target.classList.contains("bulb-on")){ // If has class bulb-on, remove it and add bulb-off
       e.target.classList.remove("bulb-on")
       e.target.classList.add("bulb-off")
-    }else if (e.target.classList.contains("bulb-off")){
+    }else if (e.target.classList.contains("bulb-off")){ // If has class bulb-ooff, remove it and add bulb-on
       e.target.classList.remove("bulb-off")
       e.target.classList.add("bulb-on")
     }
